cf1e57d7a9df5a65fe69dc13ecd54c0775d4e770
Shefali-Sharma <shefali92@gmail.com>
2018-06-13 23:50:26 +0530

    Adding command declaration
    Adding command declaration

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command:', command);
+console.log('Yargs:', argv);
+
+if(command == 'add'){
+  console.log('Add a user');
+}
+else if(command == 'remove'){
+  console.log('Remove a user');
+}
+else if(command == 'getall'){
+  console.log('Getting all users');
+}
+else{
+  console.log('Command not recognized');
+}
